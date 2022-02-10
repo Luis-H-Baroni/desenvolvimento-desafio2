3696320c69d352e5db7df378a12a73bbba1a3ac9
Luis-H-Baroni <baroniluishenrique@gmail.com>
2022-02-10 15:14:41 -0300

    Add better comments

--- API/controllers/blockchainController.js
+++ API/controllers/blockchainController.js
@@ -1,6 +1,8 @@
 const { Wallets, Gateway } = require("fabric-network")
 const fs = require("fs")
 const path = require("path")
+const Usuario = require("../models/usuarioModel")
+const Network = require("../addNetwork")
 
 // capture network variables from config.json
 const configPath = path.join(process.cwd(), "config.json")
@@ -40,35 +42,102 @@ const userExists = async (req, res) => {
 	}
 }
 
+//creates a user on offchain db, generates a id, and then register the user on blockchain with that id
 const createUser = async (req, res) => {
-	const { userId, value } = req.body
-	try {
-		const walletPath = path.join(process.cwd(), "wallet")
-		const wallet = await Wallets.newFileSystemWallet(walletPath)
-
-		const gateway = new Gateway()
-		await gateway.connect(ccp, {
-			wallet,
-			identity: "Org1 Admin",
-			discovery: { enabled: true, asLocalhost: true },
-		})
-
-		const network = await gateway.getNetwork("mychannel")
-
-		const contract = await network.getContract("smart-contract")
-
-		const response = await contract.submitTransaction(
-			"createUsuario",
-			userId,
-			value
-		)
-
-		console.log(response)
-
-		res.status(200).json(JSON.parse(response.toString()))
-	} catch (error) {
-		res.status(400).json(error)
+	//generates a random 6 number id
+	const getIdNumber = () => {
+		var n = Math.floor(Math.random() * 1000000)
+		if (n < 100000) {
+			return getIdNumber()
+		} else {
+			return n
+		}
 	}
+
+	const payload = {
+		userID: String(getIdNumber()),
+		nome: req.body.nome,
+		email: req.body.email,
+		rh: req.body.rh,
+	}
+	console.log("Payload to be sent:")
+	console.log(payload)
+
+	//create user on offchain db
+	await Usuario.create(payload).then((response) => {
+		console.log("1 - Create offchain db register")
+		if (
+			typeof response === "object" &&
+			"error" in response &&
+			response.error !== null
+		) {
+			console.log("1 - Error")
+			return res.status(500).json({
+				error: response.error,
+			})
+		} else {
+			//check if 'rh' flag is true
+			if (payload.rh) {
+				//register user on blockchain with 'rh' attributes
+				console.log("2 - Register RH on network")
+				Network.registerRhOnNetwork(payload.userID, payload.rh).then(
+					(response) => {
+						//return error if error in response
+						if (
+							typeof response === "object" &&
+							"error" in response &&
+							response.error !== null
+						) {
+							console.log(response.error)
+							res.status(500).json({
+								error: response.error,
+							})
+
+							//deletes previous created user
+							Usuario.findOneAndDelete({ userID: payload.userID }).then(() => {
+								console.log("2 - Error")
+								console.log("2 - Delete offchain db register")
+								return
+							})
+						} else {
+							//else return success
+							console.log("2 - Registered on network")
+							return res.status(201).json({ success: response })
+						}
+					}
+				)
+			} else {
+				//register user on blockchain without 'rh' attributes
+				console.log("2 - Register on network")
+				Network.registerOnNetwork(payload.userID, payload.rh).then(
+					(response) => {
+						//return error if error in response
+						if (
+							typeof response === "object" &&
+							"error" in response &&
+							response.error !== null
+						) {
+							console.log(response.error)
+							res.status(500).json({
+								error: response.error,
+							})
+
+							//deletes previous created user
+							Usuario.findOneAndDelete({ userID: payload.userID }).then(() => {
+								console.log("2 - Error")
+								console.log("2 - Delete offchain db register")
+								return
+							})
+						} else {
+							//else return success
+							console.log("2 - Registered on network")
+							return res.status(201).json({ success: response })
+						}
+					}
+				)
+			}
+		}
+	})
 }
 
 const readUser = async (req, res) => {
